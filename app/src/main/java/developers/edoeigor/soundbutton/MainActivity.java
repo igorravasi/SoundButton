29ed1dba4fea21f9ed6fd31c895ee7d9a84e34e2
Admin <edoardogullo@libero.it>
2017-04-01 22:26:12 +0200

    mplayer

--- app/src/main/java/developers/edoeigor/soundbutton/MainActivity.java
+++ app/src/main/java/developers/edoeigor/soundbutton/MainActivity.java
@@ -3,12 +3,14 @@ package developers.edoeigor.soundbutton;
 import android.media.MediaPlayer;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.KeyEvent;
 import android.view.View;
 import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
     boolean perla=false;
+    MediaPlayer mp;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -16,7 +18,7 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         final Button pulsante = (Button)findViewById(R.id.pulsante_0);
-        final MediaPlayer mp = MediaPlayer.create(this, R.raw.audio);
+        mp = MediaPlayer.create(this, R.raw.audio);
 
         pulsante.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -28,8 +30,6 @@ public class MainActivity extends AppCompatActivity {
 
     }
 
-    //ciao
-
     private int peerla_switch(){
         perla = !perla;
         if(perla){
@@ -39,4 +39,15 @@ public class MainActivity extends AppCompatActivity {
         }
     }
 
+    @Override
+    public boolean onKeyDown(int keyCode, KeyEvent event) {
+
+        if(keyCode == KeyEvent.KEYCODE_VOLUME_DOWN){
+
+            mp.start();
+
+        }
+
+        return true;
+    }
 }
