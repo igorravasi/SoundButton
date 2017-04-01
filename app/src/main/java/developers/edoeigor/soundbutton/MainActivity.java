721e97b6c97b5ffc256ff821b7413d5fd46aecec
Admin <edoardogullo@libero.it>
2017-04-01 22:04:37 +0200

    pulsante ignorante

--- app/src/main/java/developers/edoeigor/soundbutton/MainActivity.java
+++ app/src/main/java/developers/edoeigor/soundbutton/MainActivity.java
@@ -7,16 +7,27 @@ import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
+    boolean perla=false;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
         final Button pulsante = (Button)findViewById(R.id.pulsante_0);
+
         pulsante.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                pulsante.setText(getResources().getString(R.string.btn_text));
+                if (!perla){
+                    pulsante.setText(getResources().getString(R.string.btn_text));
+                    perla=true;
+                }
+                else{
+                    pulsante.setText("Cojone");
+                    perla=false;
+                }
+
             }
         });
 
